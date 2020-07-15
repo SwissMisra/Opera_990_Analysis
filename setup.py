1e12b1c2ddb32807621ff100cd35f1509bc8b5f0
Siddhartha Misra <misra.siddhartha@gmail.com>
2020-07-15 15:51:58 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project analyzes several statistics using the IRS Form 990 amongst nonprofit organizations with NTEE code 'A6A', or 'Performing Arts: Opera'.',
+    author='Siddhartha Misra',
+    license='BSD-3',
+)
